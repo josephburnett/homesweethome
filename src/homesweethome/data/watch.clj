1e97335f8a451ef29968de2d307be6d547bf4071
Joseph Burnett <josephburnett79@gmail.com>
2013-12-17 17:22:46 -0800

    Running watches.

--- src/homesweethome/data/watch.clj
+++ src/homesweethome/data/watch.clj
@@ -1,22 +1,24 @@
 (ns homesweethome.data.watch
   (:require [clojure-watch.core :refer [start-watch]]
             [homesweethome.data.config :refer [read-config]]
-            [homesweethome.data.entities.pdf :refer [intake :as intake-pdf]]))
+            [homesweethome.data.entities.pdf :refer [intake]]
+            [me.raynes.fs :refer [expand-home]]))
 
 (def intake-watches
   {"pdf" {:event-types [:create]
-          :callback (fn [event filename] (intake-pdf filename))}})
+          :callback (fn [event filename] (intake filename))}})
 
 (defn configure-watches [config]
   (map #(let [type (get % "type")
               intake-watch (get intake-watches type)]
-          (if (nil? entity-watch)
+          (if (nil? intake-watch)
             (throw (Exception. (str "Invalid watch type: " type)))
             (assoc intake-watch
-                   :path (get % "path")))
-       (get config "intake-watches"))))
+                   :path (.getAbsolutePath (expand-home (get % "path"))))))
+       (get config "intake-watches")))
 
 (defn init []
   (let [watches (configure-watches (read-config))]
+    (println "Starting watches: " watches)
     (start-watch watches)))
  
