079465e10c8c95b4ecbc6adc245779eefcba2bfe
Joseph Burnett <josephburnett79@gmail.com>
2013-12-17 21:03:23 -0800

    Add readpdf shell script and data.entities.pdf/readpdf wrapper.

--- src/homesweethome/data/entities/pdf.clj
+++ src/homesweethome/data/entities/pdf.clj
@@ -1,4 +1,12 @@
-(ns homesweethome.data.entities.pdf)
+(ns homesweethome.data.entities.pdf
+  (:require [me.raynes.fs :refer [exec]]))
 
 (defn intake [filename]
   (println "Intaking PDF: " filename))
+
+(defn readpdf [filename]
+  (let [pdftext (exec "resources/bin/readpdf" filename)]
+    (if (= 0 (:exit pdftext))
+      (:out pdftext)
+      (throw (Exception. (str "Non zero readpdf exit code: " (:exit pdftext)))))))
+ 
