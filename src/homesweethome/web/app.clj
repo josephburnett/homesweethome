c25931e2c0acedc15debcfa522bea68f0848e2a3
Joseph Burnett <josephburnett79@gmail.com>
2014-01-02 09:45:03 -0800

    Use parameters to accept key and key-prefix instead of the URL.

--- src/homesweethome/web/app.clj
+++ src/homesweethome/web/app.clj
@@ -3,21 +3,26 @@
             [homesweethome.data.entities.pdf :as pdf-entity]
             [homesweethome.web.page.pdf :as pdf-view]
             [liberator.core :refer [resource defresource]]
+            [ring.middleware.params :refer [wrap-params]]
             [compojure.core :refer [defroutes ANY]]
             [hiccup.page :refer [html5]]))
 
-(defresource view-pdf [key]
+(defresource view-pdf
   :allowed-methods [:get]
   :available-media-types ["text/html"]
-  :handle-ok (fn [_] (html5 (map #(pdf-view/render %) 
-                                 (pdf-entity/load-by-key key)))))
+  :handle-ok 
+  (fn [ctx] (let [key (get-in ctx [:request :params :key])]
+              (html5 (map #(pdf-view/render %) 
+                          (pdf-entity/load-by-key key))))))
 
-(defresource browse-pdf [key-prefix]
+(defresource browse-pdf
   :allowed-methods [:get]
   :available-media-types ["text/html"]
-  :handle-ok (fn [_] (html5 (pdf-view/render-ul 
-                              (pdf-entity/load-by-key-prefix key-prefix)))))
+  :handle-ok 
+  (fn [ctx] (let [key-prefix (get-in ctx [:request :params :key-prefix])]
+              (html5 (pdf-view/render-ul 
+                       (pdf-entity/load-by-key-prefix key-prefix))))))
 
 (defroutes app
-  (ANY "/pdf/view/:key" [key] (view-pdf key))
-  (ANY "/pdf/browse/:key-prefix" [key-prefix] (browse-pdf key-prefix)))
+  (ANY "/pdf/view" [] (wrap-params view-pdf))
+  (ANY "/pdf/browse" [] (wrap-params browse-pdf)))
