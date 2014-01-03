6387e0a316dedfeb10d1e6d1273f6feb242bb246
Joseph Burnett <josephburnett79@gmail.com>
2014-01-03 08:37:46 -0800

    Push all the view logic into the web.page.pdf namespace.

--- src/homesweethome/web/app.clj
+++ src/homesweethome/web/app.clj
@@ -4,26 +4,23 @@
             [homesweethome.web.page.pdf :as pdf-view]
             [liberator.core :refer [resource defresource]]
             [ring.middleware.params :refer [wrap-params]]
-            [compojure.core :refer [defroutes ANY]]
-            [hiccup.page :refer [html5]]))
+            [compojure.core :refer [defroutes ANY]]))
 
 (defresource view-pdf
   :allowed-methods [:get]
   :available-media-types ["text/html"]
   :handle-ok 
   (fn [ctx] (let [key (get-in ctx [:request :params "key"])]
-              (println "key:" key)
-              (html5 (map #(pdf-view/render %) 
-                          (pdf-entity/load-by-key key))))))
+              (pdf-view/render
+                (pdf-entity/load-by-key key)))))
 
 (defresource browse-pdf
   :allowed-methods [:get]
   :available-media-types ["text/html"]
   :handle-ok 
   (fn [ctx] (let [key-prefix (get-in ctx [:request :params "key-prefix"])]
-              (println "key-prefix:" key-prefix)
-              (html5 (pdf-view/render-ul 
-                       (pdf-entity/load-by-key-prefix key-prefix))))))
+              (pdf-view/render-preview
+                (pdf-entity/load-by-key-prefix key-prefix)))))
 
 (defroutes app
   (ANY "/pdf/view" [] (wrap-params view-pdf))
--- src/homesweethome/web/page/pdf.clj
+++ src/homesweethome/web/page/pdf.clj
@@ -1,6 +1,7 @@
 (ns homesweethome.web.page.pdf
   (:require [homesweethome.config :refer [entity-path]]
             [hiccup.core :refer [html]]
+            [hiccup.page :refer [html5]]
             [me.raynes.fs :refer [absolute-path expand-home]]))
 
 (def pdf-store (entity-path :pdf))
@@ -9,20 +10,18 @@
 (defn clean-text [text]
   (apply str (take text-preview-length (re-seq #"[\d\w\s\.\-]" text))))
 
-(defn render [pdf]
-  (html
-    [:div {:id (:key pdf) :class "pdf"}
-     [:span {:class "pdf-key"} (:key pdf)]
-     [:span {:class "pdf-folder"} (expand-home (str pdf-store (:file pdf)))]]))
-     ; TODO build pdf-folder in a generic way
+(defn render [pdfs]
+  (html5
+    (map #(html
+            [:div {:id (:key %) :class "pdf"}
+             [:span {:class "pdf-key"} (:key %)]
+             [:span {:class "pdf-folder"} (expand-home (str pdf-store (:file %)))]])
+         pdfs)))
      
-(defn render-li [pdf]
-  (html
-    [:div {:id (:key pdf) :class "pdf-li"}
-     [:span {:class "pdf-li-key"} (:key pdf)]
-     [:span {:class "pdf-li-text"} (clean-text (:text pdf))]]))
-
-(defn render-ul [pdf-list]
-  (html
-    [:div {:class "pdf-ul"}
-     [:ul (map #(render-li %) pdf-list)]]))
+(defn render-preview [pdfs]
+  (html5
+    (map #(html
+           [:div {:id (:key %) :class "pdf-li"}
+            [:span {:class "pdf-preview-key"} (:key %)]
+            [:span {:class "pdf-preview-text"} (clean-text (:text %))]])
+         pdfs)))
