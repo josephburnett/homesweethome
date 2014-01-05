97dc136406f391b16ccae0328d0c2fb6c5ac4462
Joseph Burnett <josephburnett79@gmail.com>
2014-01-05 14:19:11 -0800

    Fix categories to return a sorted list and include all directories.

--- src/homesweethome/data/category.clj
+++ src/homesweethome/data/category.clj
@@ -2,14 +2,16 @@
   (:require [homesweethome.config :refer [entity-path]]
             [me.raynes.fs :refer [directory? iterate-dir]]))
 
+(def hsh-folder "/.homesweethome")
+
 (defn categories [type]
   (let [path (entity-path type)]
     ;; use fs to list directories
-    (map #(.substring % 1)
-         (filter #(and (not (.isEmpty %))
-                       (not (.startsWith % "/.homesweethome")))
-                 (map (comp
-                        #(.substring % (count path))
-                        #(.getAbsolutePath %) 
-                        first)
-                      (iterate-dir (entity-path :pdf)))))))
+    (sort (map #(.substring % (+ 1 (count hsh-folder)))
+               (filter #(and (.startsWith % hsh-folder)
+                             (not (= % hsh-folder)))
+                       (map (comp
+                              #(.substring % (count path))
+                              #(.getAbsolutePath %) 
+                              first)
+                            (iterate-dir (entity-path type))))))))
